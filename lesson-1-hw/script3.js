5ecb268d5c90793d8c866620b91473d73b915275
mikhail-86 <mikel-86@yandex.ru>
2018-10-05 14:44:33 +0300

    Добавил методы добавления и удаления добавок и методы расчета стоимости и калорий.
    Не понял, как нужно добавлять исключения и как использовать константы.

--- lesson-1-hw/script3.js
+++ lesson-1-hw/script3.js
@@ -2,7 +2,7 @@
 function Hamburger (size, stuffing) {
     this.size = size;
     this.stuffing = [stuffing];
-    this.toppings = [];
+    this.topping = [];
     this.addStuffing = function (newStuffing) {
         for (let i = 0; i < this.stuffing.length; i++) {
             if (newStuffing === this.stuffing[i]) {
@@ -18,14 +18,113 @@ function Hamburger (size, stuffing) {
                 this.stuffing.splice(i,1);
             }
         }
+    };
+    this.addTopping = function (newTopping) {
+        for (let i = 0; i < this.topping.length; i++) {
+            if (newTopping === this.topping[i]) {
+                console.error('Такая добавка уже есть!');
+                return
+            } 
+        }
+        this.topping.push(newTopping);
+    };
+    this.removeTopping = function (deletedTopping) {
+        for (let i = 0; i < this.topping.length; i++) {
+            if (this.topping[i] === deletedTopping) {
+                this.topping.splice(i,1);
+            }
+        }
+    };
+    this.calculatePrice = function () {
+        var result = 0;
+		if (this.size === 'small') {
+            result +=50;
+        } else if (size == 'large') {
+            result += 100;
+        } else {
+            console.error('Размер задан не верно!');
+            return
+        }
+		
+        for (let i = 0; i < this.stuffing.length; i++) {
+            if (this.stuffing[i] === 'cheese') {
+                result += 10;
+            }   else if (this.stuffing[i] === 'salad') {
+                result += 20;
+            }   else if (this.stuffing[i] === 'potato') {
+                result += 15;
+            }   else {
+                console.error('Данной начинки нет в ассортименте!');
+                return
+            }
+        }
+        
+        for (let i = 0; i < this.topping.length; i++) {
+            if (this.topping[i] === 'mayo') {
+                result += 20;
+            }   else if (this.topping[i] === 'flavouring') {
+                result += 15;
+            }   else  {
+                console.error('Данной добавки нет в ассортименте!');
+                return
+            }
+        }
+        
+		return result;
+	};
+    this.calculateCalories = function () {
+        var result = 0;
+		if (this.size === 'small') {
+            result +=20;
+        } else if (size == 'large') {
+            result += 40;
+        } else {
+            console.error('Размер задан не верно!');
+            return
+        }
+		
+        for (let i = 0; i < this.stuffing.length; i++) {
+            if (this.stuffing[i] === 'cheese') {
+                result += 20;
+            }   else if (this.stuffing[i] === 'salad') {
+                result += 5;
+            }   else if (this.stuffing[i] === 'potato') {
+                result += 10;
+            }   else {
+                console.error('Данной начинки нет в ассортименте!');
+                return
+            }
+        }
+        
+        for (let i = 0; i < this.topping.length; i++) {
+            if (this.topping[i] === 'mayo') {
+                result += 5;
+            }   else if (this.topping[i] === 'flavouring') {
+                result += 0;
+            }   else  {
+                console.error('Данной добавки нет в ассортименте!');
+                return
+            }
+        }
+        
+		return result;
     }
-}
+};
+
 
 let Ham1 = new Hamburger('small', 'cheese');
-console.log(Ham1.stuffing);
+
 Ham1.addStuffing('potato');
-console.log(Ham1.stuffing);
 Ham1.addStuffing('salad');
 console.log(Ham1.stuffing);
 Ham1.removeStuffing('salad');
 console.log(Ham1.stuffing);
+Ham1.addTopping('mayo');
+console.log(Ham1.topping);
+Ham1.removeTopping('mayo');
+console.log(Ham1.topping);
+Ham1.addTopping('flavouring');
+console.log(Ham1.topping);
+console.log('Цена гамбургера составит: ' + Ham1.calculatePrice() + ' рублей.');
+console.log('Количество калорий составит: ' + Ham1.calculateCalories() + ' калорий.');
+
