ceb8456152eca9076993588a8af3e958cd7a05c3
mikhail-86 <mikel-86@yandex.ru>
2018-10-26 08:34:05 +0300

    lesson-7-hw

---
+++ lesson-7-hw/src/some.js
@@ -0,0 +1,10 @@
+function sum (...numbers) {
+    return numbers.reduce((acc, item) => {
+       return acc + item 
+    }, 0);
+}
+function avg(...numbers) {
+    return sum(...numbers)/numbers.length;
+}
+
+export default avg;
